9865259b374e3a0daca8cbaf01590b849554a2a2
betojaton <betojaton@gmail.com>
2020-11-06 12:59:51 -0300

    Creacion de la version HTML

---
+++ v01-html/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<title>betojaton | Diseñador Freelancer Senior UX/UI</title>
+	<meta property="og:image" content="img/favicon-192x192.png">
+	<meta property="og:type" content="website">
+	<meta name="description" content="Sitio Web personal de Roberto Jaton, diseñador Freelancer senior con experiencia en UX/UI, WordPress, logotipos, front-end.">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="img/favicon-192x192.png">
+	<link rel="icon" type="image/png" sizes="512x512" href="img/favicon-512x512.png">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;300;400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
+	<!--PLUGINS-->
+	<link href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" rel="stylesheet">-->
+	<link href="css/fullpage.min.css" rel="stylesheet" type="text/css">
+	<link href="css/custom.css" rel="stylesheet" >
+	<!-- <link href="css/hover-min.css" rel="stylesheet"> -->
+</head>
---
+++ v01-html/sidebar.php
@@ -0,0 +1,44 @@
+<aside class="sidebar" role="complementary" >
+	<nav class="navbar fixed-top off-canvas" >
+		<div class="bg-skew shadow-lg"></div>
+		<header class="d-flex flex-column flex-wrap justify-content-start" role="banner">
+                  <!--=============================
+                  =            BUTTONs            =
+                  ==============================-->
+                  <button class="btn btn-open drawer-knob bg-secondary rounded-circle" type="button" data-open="drawer">
+                  	<i class="fas fa-bars text-white"></i>
+                  </button>
+                  <div class="text-center d-flex justify-content-between brand-line mb-auto"></div>
+                  <a href="#page1" title="Volver al Inicio" class="navbar-brand my-lg-4 mb-2"><h1 class="sr-only">betojaton</h1></a>
+                  <button class="btn btn-close bg-secondary rounded-circle" type="button" data-dismiss="drawer">
+                  	<span class="sr-only">Toggle Navigation </span><i class="fas fa-long-arrow-alt-left text-white"></i>
+                  </button>
+                  <!--====  End of BUTTONs  ====-->
+
+                  <!--=================================
+                  =            NAVBAR PPAL            =
+                  ==================================-->
+                  <ul class="nav navbar-ppal navbar-nav drawer-menu my-auto pl-sm-3 pl-lg-5 w-75" id="menu-ppal" role="navigation">
+                  	<li role="presentation" class="nav-item overflow-hidden">
+                  		<a href="#page2" title="Seccion Portfolio" class="nav-link effect hover-skew">
+                                    Portfolio
+                              </a>
+                        </li>
+                        <li role="presentation" class="nav-item">
+                              <a href="#page3" title="Seccion Sobre Mi" class="nav-link effect hover-skew">
+                                    Sobre Mi
+                              </a>
+                        </li>
+                        <li role="presentation" class="nav-item">
+                              <a href="#page4" title="Seccion Servicios" class="nav-link effect hover-skew">
+                                    Servicios
+                              </a>
+                        </li>
+                  	<!-- <li role="presentation" class="nav-item">
+                  		<a href="#page2/slide1-4" title="Seccion Blog" class="nav-link effect hover-skew">Blog</a>
+                  	</li> -->
+                  </ul>
+                  <!--====  End of NAVBAR PPAL  ====-->
+            </header>
+      </nav>
+</aside>
